70c82fa0675f475e0d5ab1b1e328a8bb77132974
yinxiu zhan <yinxiuzhan@F346M-BD8E6A.local>
2021-09-01 10:05:16 +0200

    :hammer: Refactor core R wrapper

--- CaTCH/R/core.R
+++ CaTCH/R/core.R
@@ -1,60 +1,56 @@
-domain.call <- function (input){
-	hicData=read.table(input,nrows=10)
-	ncolumns=ncol(hicData)
-	
-	#check that each file contains a single chromosome
-	stopifnot(length(unique(hicData[,1]))==1) 
+domain.call <- function(input) { # nolint
+        hicdata <- read.table(input, nrows = 10)
+        ncolumns <- ncol(hicdata)
 
-	#check type of file
-	if(ncolumns==6){
-	  original=read.table(input,colClasses=c("character","numeric","numeric","numeric","numeric","numeric"))
-	  hicData=original[,c(-2,-3)]
-	}else if(ncolumns==4){
-	  original=read.table(input,colClasses=c("character","numeric","numeric","numeric"))
-	  hicData=original
-	}else{
-	  stop("input file has the wrong format. It needs 4 or 6 columns")	
-	}
-	
-	#calling C function
-	res <- .Call("Catch", as.matrix(hicData[,-1]), PACKAGE="CaTCH")
-	colnames(res$ncluster) <- c("RI","ndomains")
-	colnames(res$clusters) <- c("RI","start","end","insulation")
-	res$ncluster <- data.frame(chromosome=unique(hicData[,1]),res$ncluster)
-	res$clusters <- data.frame(chromosome=unique(hicData[,1]),res$clusters)
-	return(res)
+        # check that each file contains a single chromosome
+        stopifnot(length(unique(hicdata[, 1])) == 1)
+
+        # check type of file
+        if (ncolumns == 6) {
+                original <- read.table(input, colClasses = c("character", "numeric", "numeric", "numeric", "numeric", "numeric")) # nolint
+                hicdata <- original[, c(-2, -3)]
+        } else if (ncolumns == 4) {
+                original <- read.table(input, colClasses = c("character", "numeric", "numeric", "numeric")) # nolint
+                hicdata <- original
+        } else {
+                stop("input file has the wrong format. It needs 4 or 6 columns")
+        }
+
+        # calling C function
+        res <- .Call("Catch", as.matrix(hicdata[, -1]), PACKAGE = "CaTCH")
+        colnames(res$ncluster) <- c("RI", "ndomains")
+        colnames(res$clusters) <- c("RI", "start", "end", "insulation")
+        res$ncluster <- data.frame(chromosome = unique(hicdata[, 1]), res$ncluster) # nolint
+        res$clusters <- data.frame(chromosome = unique(hicdata[, 1]), res$clusters) # nolint
+        return(res)
 }
 
-domain.call.parallel <- function(inputs, ncpu=4) {
-	# check input parameters
-	stopifnot(is.numeric(ncpu) || ncpu < 1L)
-	stopifnot(is.character(inputs))
-	stopifnot(all(file.exists(inputs)))
+domain.call.parallel <- function(inputs, ncpu = 4) { # nolint
+        # check input parameters
+        stopifnot(is.numeric(ncpu) || ncpu < 1L)
+        stopifnot(is.character(inputs))
+        stopifnot(all(file.exists(inputs)))
 
-	# start parallel R processes
-	cl <- parallel::makeCluster(min(ncpu, length(inputs)))
-	message("loading CaTCH on ",length(cl)," nodes...", appendLF=FALSE)
-	res <- parallel::clusterEvalQ(cl, library(CaTCH))
-	if(!all(sapply(res, function(x) "CaTCH" %in% x)))
-		stop("'CaTCH' could not be loaded on all cluster nodes")
-	message("done")
+        # start parallel R processes
+        cl <- parallel::makeCluster(min(ncpu, length(inputs)))
+        message("loading CaTCH on ", length(cl), " nodes...", appendLF = FALSE)
+        res <- parallel::clusterEvalQ(cl, library(CaTCH))
+        if (!all(sapply(res, function(x) "CaTCH" %in% x))) {
+                stop("'CaTCH' could not be loaded on all cluster nodes")
+        }
+        message("done")
 
-	# segment chromosomes
-	res <- parLapply(cl, inputs, domain.call)
-	#res <- parLapply(cl, inputs, function(x) {
-	#	message("now segmenting ", x)
-	#	data.frame(chr=basename(x), start=1:3*10, end=2:4*10 -1)
-	#})
+        # segment chromosomes
+        res <- parLapply(cl, inputs, domain.call) # nolint
 
-	# post-process results list
-	#res <- do.call(rbind, res)
-	resComb <- list(clusters=do.call(rbind, lapply(res, "[[", "clusters")),
-		        ncluster=do.call(rbind, lapply(res, "[[", "ncluster")))
+        res_comb <- list(
+                clusters = do.call(rbind, lapply(res, "[[", "clusters")),
+                ncluster = do.call(rbind, lapply(res, "[[", "ncluster"))
+        )
 
-	# stop cluster
-	parallel::stopCluster(cl)
+        # stop cluster
+        parallel::stopCluster(cl)
 
-	# return result
-	return(res)
+        # return result
+        return(res)
 }
-
